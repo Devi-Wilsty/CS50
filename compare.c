c1fa63f89ceeb1d44f701b0596dfadeff242507b
Devi-Wilsty <devinwilsterman0614@gmail.com>
2025-08-07 15:29:42 +0000

    Commit message: Update project files and settings

---
+++ compare.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+
+int main(void) 
+{
+    int x = get_int("what's x? ");
+    int y = get_int("what's y? ");
+
+    if (x < y) 
+    {
+        printf("x is less than y\n");
+    }
+    if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    if (x == y)
+    {
+        printf("x is equal to y\n");
+    }
+}
